e5b6dfc47dee975c53dcdca0b91702fd122c552b
JangWoojun <woojun0107@naver.com>
2024-08-25 03:06:32 +0900

    feat: SplashActivity 이동 로직 구현

--- app/src/main/java/com/seogaemo/hackseoul_android/view/SplashActivity.kt
+++ app/src/main/java/com/seogaemo/hackseoul_android/view/SplashActivity.kt
@@ -1,21 +1,21 @@
 package com.seogaemo.hackseoul_android.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.seogaemo.hackseoul_android.R
+import com.seogaemo.hackseoul_android.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finishAffinity()
+        }, 1500)
     }
 }
