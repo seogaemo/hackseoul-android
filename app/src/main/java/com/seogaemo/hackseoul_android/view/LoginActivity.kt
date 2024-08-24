99920fc4ee29e1f3d4343371ba78f999ad213e78
JangWoojun <woojun0107@naver.com>
2024-08-25 05:21:21 +0900

    feat: LoginActivity 로직 완료

--- app/src/main/java/com/seogaemo/hackseoul_android/view/LoginActivity.kt
+++ app/src/main/java/com/seogaemo/hackseoul_android/view/LoginActivity.kt
@@ -1,21 +1,25 @@
 package com.seogaemo.hackseoul_android.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.seogaemo.hackseoul_android.R
+import com.seogaemo.hackseoul_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            if (binding.idInput.text.isNotEmpty() && binding.passwordInput.text.isNotEmpty()) {
+                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                finishAffinity()
+            } else {
+                Toast.makeText(this@LoginActivity, "필수 입력 필드를 채워주세요.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
