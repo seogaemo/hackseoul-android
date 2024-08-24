20194a6a6253fc4c6f59d7f598a3a18c950a7815
JangWoojun <woojun0107@naver.com>
2024-08-25 04:50:02 +0900

    feat: DistributionAdapter 추가

--- app/src/main/java/com/seogaemo/hackseoul_android/adapter/DistributionAdapter.kt
+++ app/src/main/java/com/seogaemo/hackseoul_android/adapter/DistributionAdapter.kt
@@ -1,17 +1,24 @@
 package com.seogaemo.hackseoul_android.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.seogaemo.hackseoul_android.data.Distribution
 import com.seogaemo.hackseoul_android.databinding.DistributionItemBinding
+import com.seogaemo.hackseoul_android.view.DistributionActivity
 
 class DistributionAdapter (private val distributionList: List<Distribution>):
     RecyclerView.Adapter<DistributionAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = DistributionItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return ViewHolder(binding)
+        return ViewHolder(binding).also { hanler ->
+            binding.root.setOnClickListener {
+                val context = binding.root.context
+                context.startActivity(Intent(context, DistributionActivity::class.java))
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
