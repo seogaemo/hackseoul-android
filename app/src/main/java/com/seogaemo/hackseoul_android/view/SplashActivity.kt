1f7b943bd162a1f680a7dbe56bd3ddc06d84be27
JangWoojun <woojun0107@naver.com>
2024-08-25 10:52:17 +0900

    feat: 딥링크 이동 구현

--- app/src/main/java/com/seogaemo/hackseoul_android/view/SplashActivity.kt
+++ app/src/main/java/com/seogaemo/hackseoul_android/view/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.seogaemo.hackseoul_android.view
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,17 +11,34 @@ import com.seogaemo.hackseoul_android.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        handlingActivities()
+    }
+
+    private fun handlingActivities() {
+        val action: String? = intent?.action
+        val data: Uri? = intent?.data
+
         Handler(Looper.getMainLooper()).postDelayed({
             if (SharedPreference.isFirst) {
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            } else {
                 startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            } else {
+                if (action == Intent.ACTION_VIEW) {
+                    val id = data?.getQueryParameter("id")
+                    val intent = Intent(this@SplashActivity, ProductActivity::class.java)
+                    intent.putExtra("id", id)
+                    startActivity(intent)
+                } else {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                }
             }
             finishAffinity()
         }, 1500)
     }
+
 }
