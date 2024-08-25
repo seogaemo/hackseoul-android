a2c41afc8f2acf182322f67328d01dc2709d6657
JangWoojun <woojun0107@naver.com>
2024-08-25 09:41:03 +0900

    feat: 로그인 기능 구현

--- app/src/main/java/com/seogaemo/hackseoul_android/view/LoginActivity.kt
+++ app/src/main/java/com/seogaemo/hackseoul_android/view/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.seogaemo.hackseoul_android.view
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.seogaemo.hackseoul_android.R
+import com.seogaemo.hackseoul_android.data.SignInRequest
+import com.seogaemo.hackseoul_android.data.SignInResponse
+import com.seogaemo.hackseoul_android.database.SharedPreference
 import com.seogaemo.hackseoul_android.databinding.ActivityLoginBinding
+import com.seogaemo.hackseoul_android.network.RetrofitAPI
+import com.seogaemo.hackseoul_android.network.RetrofitClient
 import com.seogaemo.hackseoul_android.util.BaseActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginActivity : BaseActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -37,11 +47,42 @@ class LoginActivity : BaseActivity() {
 
         binding.loginButton.setOnClickListener {
             if (binding.idInput.text.isNotEmpty() && binding.passwordInput.text.isNotEmpty()) {
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                finishAffinity()
+                lifecycleScope.launch {
+                    val token = postSignIn(this@LoginActivity, SignInRequest(binding.idInput.text.toString(), binding.passwordInput.text.toString()))
+                    if (token != null) {
+                        SharedPreference.token = token.token
+                        SharedPreference.isFirst = false
+
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        finishAffinity()
+                    }
+                }
             } else {
                 Toast.makeText(this@LoginActivity, "필수 입력 필드를 채워주세요.", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    private suspend fun postSignIn(context: Context, signInRequest: SignInRequest): SignInResponse? {
+        return try {
+            withContext(Dispatchers.IO) {
+                val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
+                val response = retrofitAPI.signIn(signInRequest)
+                if (response.isSuccessful) {
+                    response.body()
+                } else {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(context, "로그인에 실패하였습니다", Toast.LENGTH_SHORT).show()
+                    }
+                    null
+                }
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "로그인에 실패하였습니다", Toast.LENGTH_SHORT).show()
+            }
+            null
+        }
+    }
+
 }
