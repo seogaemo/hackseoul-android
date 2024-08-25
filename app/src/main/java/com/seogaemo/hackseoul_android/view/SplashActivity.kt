5f9b44620087e9255a222cd4f7a1c02c17f97170
JangWoojun <woojun0107@naver.com>
2024-08-25 09:41:15 +0900

    feat: 로그인 여부에 따른 페이지 이동 구현

--- app/src/main/java/com/seogaemo/hackseoul_android/view/SplashActivity.kt
+++ app/src/main/java/com/seogaemo/hackseoul_android/view/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.seogaemo.hackseoul_android.database.SharedPreference
 import com.seogaemo.hackseoul_android.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -14,7 +15,11 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@SplashActivity, ProductActivity::class.java))
+            if (SharedPreference.isFirst) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
             finishAffinity()
         }, 1500)
     }
